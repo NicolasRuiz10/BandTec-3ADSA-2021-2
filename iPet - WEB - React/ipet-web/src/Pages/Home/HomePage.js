c68f218bc9be4f7eadbd4187f0be108f11cce8bf
GabrielNicodemos <gabriel.nicodemos@bandtec.com.br>
2021-09-28 18:52:51 -0300

    Alterações no Home Page e no FavIcon e title

--- iPet - WEB - React/ipet-web/src/Pages/Home/HomePage.js
+++ iPet - WEB - React/ipet-web/src/Pages/Home/HomePage.js
@@ -3,6 +3,11 @@ import { Intro } from "../../components/intro/Intro";
 import { Menu } from "../../components/menu/Menu";
 import './HomePage.css';
 
+import MagnusLogo from '../../Assets/magnus.png'
+import PedigreeLogo from '../../Assets/pedigree.png'
+import PurinaLogo from '../../Assets/purina.png'
+import WhiskasLogo from '../../Assets/whiskas-logo.png'
+
 function HomePage() {
   return (
     <>
@@ -15,23 +20,27 @@ function HomePage() {
           </h1>
           <br />
           <h2 className="brandText">
-            Aqui você encontra as melhores marcas que oferecemos com suma confiançais
+            Aqui você encontra as melhores marcas que oferecemos com suma confianças
           </h2>
         </div>
         <div className="brandsImages">
           <div className="brand1">
-            <img clasName="brand1" src="logoPetz.png" height="200px" width="250px" alt="none" />
+            <img clasName="brand1" src={MagnusLogo} height="80px" width="120px" alt="none" />
           </div>
           <div className="brand2">
-            <img clasName="brand2" src="mi-au_logo.png" height="200px" width="250px" alt="none" />
+            <img clasName="brand2" src={PedigreeLogo} height="80px" width="120px" alt="none" />
           </div>
           <div className="brand3">
-            <img src="vitaprime_logo.png" height="200px" width="250px" alt="none" />
+            <img src={PurinaLogo} height="80px" width="120px" alt="none" />
           </div>
           <div className="brand4">
-            <img src="americanPets.png" height="200px" width="250px" alt="none" />
+            <img src={WhiskasLogo} height="80px" width="120px" alt="none" />
           </div>
         </div>
+      </section>
+      <section className="produtos">
+        <h3>Produtos de diversas lojas</h3>
+
       </section>
     </>
   );
