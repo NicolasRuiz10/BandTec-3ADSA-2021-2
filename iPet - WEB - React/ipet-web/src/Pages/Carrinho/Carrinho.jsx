106e5e716e27caa62ba85bb8d65a0d67b9ffbb8d
GabrielOrtelan <gabriel.montanari@bandtec.com.br>
2021-10-13 11:20:56 -0300

    carrinho produto calculando preço

--- iPet - WEB - React/ipet-web/src/Pages/Carrinho/Carrinho.jsx
+++ iPet - WEB - React/ipet-web/src/Pages/Carrinho/Carrinho.jsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import './Carrinho.css';
 import { Menu } from "../../components/menu/Menu";
 import Progress from "../../components/progress/Progress";
+import Cama from '../../Assets/cama.jpg'
+
+
 
 export function Carrinho() {
+  const product = {
+    price: 100,
+  }
+  const [quantidade, setQuantidade] = useState(0);
+  const total = product.price * quantidade;
   return (
     <>
       <Menu menuItem1="PetShop" menuItem2="Produtos" menuItem3="Serviços" />
@@ -13,6 +21,36 @@ export function Carrinho() {
         <div className="navBar--carrinho">
           <div className="progressBar">
             <Progress />
+          </div>
+        </div>
+        <div className="box-carrinho">
+          <div className="itens-carrinho">
+            <div className="item-product">
+              <h1>Produto</h1>
+              <img src={Cama} width="400" height="300" />
+            </div>
+            <div className="item-price">
+              <h1>Preço</h1>
+              <div className="item-priceNumber">R$ - {product.price}</div>
+            </div>
+            <div className="item-quantity">
+              <h1>Quantidade</h1>
+              <button onClick={() => setQuantidade(quantidade + 1)}>
+                +
+              </button>
+              <div>
+                {quantidade}
+              </div>
+              <button onClick={() => setQuantidade(quantidade - 1)}>
+                -
+              </button>
+            </div>
+            <div className="item-total">
+              <h1>Total</h1>
+              <div>
+                {total}
+              </div>
+            </div>
           </div>
         </div>
       </div>
