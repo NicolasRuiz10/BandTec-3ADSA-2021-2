19cba0ce1329c842739a05876184ff1df32fd384
Nicolas Ruiz Nogueira Xavier <nrxavier@stefanini.com>
2021-10-20 19:07:08 -0300

    att

--- Java/spring-sprint-2/src/main/java/com/br/springsprint2/controle/ProdutoController.java
+++ Java/spring-sprint-2/src/main/java/com/br/springsprint2/controle/ProdutoController.java
@@ -4,11 +4,17 @@ import com.br.springsprint2.dominio.Petshop;
 import com.br.springsprint2.dominio.Produto;
 import com.br.springsprint2.repositorio.PetshopRepository;
 import com.br.springsprint2.repositorio.ProdutoRepository;
+import com.br.springsprint2.util.ListaObj;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Formatter;
+import java.util.FormatterClosedException;
 import java.util.List;
 
 import static org.springframework.http.ResponseEntity.status;
@@ -16,11 +22,64 @@ import static org.springframework.http.ResponseEntity.status;
 @RestController
 @RequestMapping("/produtos")
 public class ProdutoController {
+
+    public static void gravaLista(ListaObj<Produto> lista, String nomeArq) {
+
+        FileWriter arq = null;
+        Formatter saida = null;
+        boolean deuRuim = false;
+
+        nomeArq += ".csv";
+
+
+        try {
+            arq = new FileWriter(nomeArq, true);
+            saida = new Formatter(arq);
+        }
+        catch (IOException erro) {
+            System.err.println("Erro ao abrir arquivo");
+            System.exit(1);
+        }
+
+
+        try {
+
+            for (int i=0; i< lista.getTamanho(); i++) {
+                Produto a = lista.getElemento(i);
+
+                saida.format("%d;%s;%s;%.2f;%s;%s;%d\n",a.getIdProduto(),
+                        a.getNome(),a.getDescricao(), a.getValor(), a.getMarca(), a.getEspecie(), a.getQuantidade());
+            }
+        }
+        catch (FormatterClosedException erro) {
+            System.err.println("Erro ao gravar no arquivo");
+            deuRuim= true;
+        }
+        finally {
+
+            saida.close();
+            try {
+                arq.close();
+            }
+            catch (IOException erro) {
+                System.err.println("Erro ao fechar arquivo.");
+                deuRuim = true;
+            }
+            if (deuRuim) {
+                System.exit(1);
+            }
+        }
+    }
+
+
     @Autowired
     private ProdutoRepository repository;
 
     @Autowired
     private PetshopRepository petRepository;
+
+    private ListaObj listaObj = new ListaObj(1000);
+
 
     @CrossOrigin
     @PostMapping("{fkPetshop}")
@@ -28,6 +87,7 @@ public class ProdutoController {
         Petshop petshop = petRepository.findById(fkPetshop).get();
         novoProduto.setFkPetShop(petshop);
         repository.save(novoProduto);
+        listaObj.adicionar(novoProduto);
         return status(HttpStatus.CREATED).build();
     }
 
@@ -35,7 +95,19 @@ public class ProdutoController {
     @GetMapping
     public ResponseEntity getProdutos() {
         List<Produto> lista = repository.findAll();
+        /*
+        List listao = new ArrayList();
+        for (int x = 0; x < listaObj.getTamanho(); x++) {
+           listao.add(listaObj.getElemento(x));
+        }*/
+
         return lista.isEmpty() ? ResponseEntity.noContent().build() : ResponseEntity.ok().body(lista);
+    }
+
+    @GetMapping("/csv/{produtos}")
+    public ResponseEntity getCSV(@PathVariable String produtos) {
+        gravaLista(listaObj , produtos);
+        return ResponseEntity.status(200).build();
     }
 
     @CrossOrigin
@@ -49,6 +121,7 @@ public class ProdutoController {
     public ResponseEntity deleteProdutos(@PathVariable int id) {
         if (repository.existsById(id)) {
             repository.deleteById(id);
+
             return ResponseEntity.status(200).build();
         } else {
             return ResponseEntity.status(404).build();
