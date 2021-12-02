6e4d9517db4fa088138aad7dd4bea4b70a46e643
felipe.olegario <felipe.olegario@bandtec.com.br>
2021-12-02 00:58:28 -0300

    up controller

--- Java/spring-sprint-2/src/main/java/com/br/springsprint2/controle/ItensPedidoController.java
+++ Java/spring-sprint-2/src/main/java/com/br/springsprint2/controle/ItensPedidoController.java
@@ -30,6 +30,7 @@ public class ItensPedidoController {
     @Autowired
     private PetshopRepository petshopRepository;
 
+    @CrossOrigin
     @GetMapping
     public ResponseEntity getAllItensPedido() {
         List<ItensPedido> listaItensPedido = itensPedidoRepository.findAll();
@@ -39,6 +40,7 @@ public class ItensPedidoController {
         return ResponseEntity.ok(listaItensPedido);
     }
 
+    @CrossOrigin
     @PostMapping("/{idProduto}")
     public ResponseEntity postItensPedido(@PathVariable Integer idProduto,@RequestBody ItensPedido itensPedido) {
         ItensPedido itensPedido1 = itensPedidoRepository.findOneById(itensPedido.getId());
@@ -50,6 +52,7 @@ public class ItensPedidoController {
         return ResponseEntity.ok(itensPedido);
     }
 
+    @CrossOrigin
     @PutMapping("/id/{id}")
     public ResponseEntity putItensPedido(
             @PathVariable Integer idItens,
@@ -65,6 +68,7 @@ public class ItensPedidoController {
         return ResponseEntity.notFound().build();
     }
 
+    @CrossOrigin
     @DeleteMapping("/id/{id}")
     public ResponseEntity deleteItensPedido(@PathVariable Integer id) {
         if (itensPedidoRepository.existsById(id)) {
@@ -74,6 +78,7 @@ public class ItensPedidoController {
         return ResponseEntity.notFound().build();
     }
 
+    @CrossOrigin
     @GetMapping("/pedido/id/{id}")
     public ResponseEntity getAllItensProdutoByPedidoId(@PathVariable Integer id) {
         if (pedidoRepository.existsById(id)) {
@@ -86,6 +91,7 @@ public class ItensPedidoController {
         return ResponseEntity.notFound().build();
     }
 
+    @CrossOrigin
     @GetMapping("/pedido/petshop/{idPetshop}")
     public ResponseEntity getAllItensProdutoByPetshopId(@PathVariable Integer idPetshop) {
         if (petshopRepository.existsById(idPetshop)) {
@@ -98,6 +104,7 @@ public class ItensPedidoController {
         return ResponseEntity.notFound().build();
     }
 
+    @CrossOrigin
     @GetMapping("/pedido/numero/{numero}")
     public ResponseEntity getAllItensPedidoByPedidoId(@PathVariable Integer id) {
         if (pedidoRepository.existsById(id)) {
