ef03646025ae26647294a5085d73cf30110b04bf
GabrielNicodemos04 <gabrielcaravalho@firecodeit.com>
2022-02-16 22:06:19 -0300

    Alteraçoes no site

--- iPet - WEB - React/ipet-web/src/Pages/Produtos/Produtos.jsx
+++ iPet - WEB - React/ipet-web/src/Pages/Produtos/Produtos.jsx
@@ -141,29 +141,12 @@ export function Produtos(props) {
         <>
             <Toast text="Login ou senha incorretos" color="green" showToast={showToast} changeValueToast={setValueToast} />
             <Menu menuItem1="PetShop" menuItem2="Produtos" menuItem3="Serviços" />
-            <div className="produto">
+            <section className="section-produtos-intro">
                 <h2>Produtos</h2>
-                {
-                    petshopProduto === 1 ?
-                        <div className="produtosPetShop">
-                            <span>Produtos do PetShop: Cobasi</span>
-                        </div>
-                        :
-                        petshopProduto === 2 ?
-                            <div className="produtosPetShop">
-                                <span>Produtos do PetShop: Petz</span>
-                            </div>
-                            :
-                            <div className="produtosPetShop">
-                                <span>Produtos do PetShop: Todos</span>
-                            </div>
-                }
-            </div>
-            <hr />
-            <div className="informacao-compra">
-                <span>* Favor comprar produtos de um único PetShop</span>
-            </div>
-            <div className="principal">
+                <hr />
+            </section>
+
+            <section className="card-empresas">
                 <div className="filtro--produtos">
                     <h3>Filtrar Produtos</h3>
                     <hr />
@@ -172,195 +155,23 @@ export function Produtos(props) {
                         value={busca}
                         placeholder="Pesquisar produto.."
                     />
-                    <h3>Pet</h3>
-                    <hr />
-                    {/* CheckBox Pet */}
-                    <div className="checkBox">
-                        <input
-                            type="checkbox"
-                            name="marcaCheck"
-                            value="Cachorro"
-                            onClick={filtroMarca}
-                        />
-                        <label for="marcaCheck">Cachorro</label>
-                    </div>
-                    <div className="checkBox">
-                        <input
-                            type="checkbox"
-                            name="marcaCheck"
-                            value="Gato"
-                            onClick={filtroMarca}
-                        />
-                        <label for="marcaCheck">Gato</label>
-                    </div>
-                    <div className="checkBox">
-                        <input
-                            type="checkbox"
-                            name="marcaCheck"
-                            value="Outros"
-                            onClick={filtroMarca}
-                        />
-                        <label for="marcaCheck">Outros</label>
-                    </div>
-                    <h3>Tipo Produtos</h3>
-                    <hr />
-                    {/* CheckBox tipoProduto */}
-                    <div className="checkBox">
-                        <input
-                            type="checkbox"
-                            name="marcaCheck"
-                            value="Ração"
-                            onClick={filtroMarca}
-                        />
-                        <label for="marcaCheck">Ração</label>
-                    </div>
-                    <div className="checkBox">
-                        <input
-                            type="checkbox"
-                            name="marcaCheck"
-                            value="Petiscos"
-                            onClick={filtroMarca}
-                        />
-                        <label for="marcaCheck">Petiscos</label>
-                    </div>
-                    <div className="checkBox">
-                        <input
-                            type="checkbox"
-                            name="marcaCheck"
-                            value="Acessório"
-                            onClick={filtroMarca}
-                        />
-                        <label for="marcaCheck">Acessórios</label>
-                    </div>
-                    <div className="checkBox">
-                        <input
-                            type="checkbox"
-                            name="marcaCheck"
-                            value="Roupas"
-                            onClick={filtroMarca}
-                        />
-                        <label for="marcaCheck">Roupas</label>
-                    </div>
-                    <div className="checkBox">
-                        <input
-                            type="checkbox"
-                            name="marcaCheck"
-                            value="Brinquedos"
-                            onClick={filtroMarca}
-                        />
-                        <label for="marcaCheck">Brinquedos</label>
-                    </div>
-                    {/* CHECKBOX MARCAS */}
-                    <h3>Marca</h3>
-                    <hr />
-                    <div className="checkBox">
-                        <input
-                            type="checkbox"
-                            name="marcaCheck"
-                            value="7"
-                            onClick={filtroMarca}
-                        />
-                        <label for="marcaCheck">Golden</label>
-                    </div>
-                    <div className="checkBox">
-                        <input
-                            type="checkbox"
-                            name="marcaCheck"
-                            value="6"
-                            onClick={filtroMarca}
-                        />
-                        <label for="marcaCheck">Me.Au</label>
-                    </div>
-                    <div className="checkBox">
-                        <input
-                            type="checkbox"
-                            name="marcaCheck"
-                            value="5"
-                            onClick={filtroMarca}
-                        />
-                        <label for="marcaCheck">Nutrilus</label>
-                    </div>
-                    <div className="checkBox">
-                        <input
-                            type="checkbox"
-                            name="marcaCheck"
-                            value="4"
-                            onClick={filtroMarca}
-                        />
-                        <label for="marcaCheck">Magnus</label>
-                    </div>
-                    <div className="checkBox">
-                        <input
-                            type="checkbox"
-                            name="marcaCheck"
-                            value="3"
-                            onClick={filtroMarca}
-                        />
-                        <label for="marcaCheck">True</label>
-                    </div>
-                    <div className="checkBox">
-                        <input
-                            type="checkbox"
-                            name="marcaCheck"
-                            value="2"
-                            onClick={filtroMarca}
-                        />
-                        <label for="marcaCheck">LCM</label>
-                    </div>
-                    <div className="checkBox">
-                        <input
-                            type="checkbox"
-                            name="marcaCheck"
-                            value="1"
-                            onClick={filtroMarca}
-                        />
-                        <label for="marcaCheck">Snacks</label>
-                    </div>
-                    <h3>PetShop</h3>
-                    <hr />
-
-                    <div className="checkBox">
-                        <input
-                            type="checkbox"
-                            name="marcaCheck"
-                            value="8"
-                            onClick={filtroMarca}
-                        />
-                        <label for="marcaCheck">Cobasi</label>
-                    </div>
-                    <div className="checkBox">
-                        <input
-                            type="checkbox"
-                            name="marcaCheck"
-                            value="9"
-                            onClick={filtroMarca}
-                        />
-                        <label for="marcaCheck">Petz</label>
-                    </div>
                 </div>
-                <div className="card--principal">
-                    {
-                        produtos.length > 0 ?
-                            produtosFiltrados.map((produto, key) => (
-                                <CardProdutos key={key} produto={produto} />
 
-                            )) :
-                            <ContentLoader
-                                className="center"
-                                speed={2}
-                                width={400}
-                                height={150}
-                                viewBox="0 0 400 150"
-                                backgroundColor="#2484ec"
-                                foregroundColor="#fec302"
-                                {...props}
-                            >
-                                <circle cx="265" cy="77" r="63" />
-                                <circle cx="141" cy="82" r="36" />
-                                <circle cx="51" cy="88" r="22" />
-                            </ContentLoader>}
+                <div className="card-empresas-info">
+                    <div className="logo-empresa">
+                        <img src="https://isinaliza.vteximg.com.br/arquivos/ids/173981-1000-1000/1943-placa-atencao-area-de-teste-pvc-semi-rigido-26x18cm-fixacao-1.jpg?v=636848934015200000" alt="" srcset="" />
+                    </div>
+                    <div className="detalhes-empresa">
+                        <h3>Teste</h3>
+                        <p>R$ 3,00 <span>Taxa de entrega</span></p>
+                        <p>40 min <span>Entrega</span></p>
+                    </div>
+                    <div className="button-empresa">
+                        <a>Ver produtos</a>
+                    </div>
+
                 </div>
-            </div>
+            </section>
         </>
     );
 }
